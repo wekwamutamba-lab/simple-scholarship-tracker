343474cf892df738af4877002c114097e280dea9
Stacy Mutamba <wekwamutamba@gmail.com>
2026-08-21 14:49:55 +0200

    final changes

--- server/backend/index.js
+++ server/backend/index.js
@@ -1,21 +1,20 @@
-require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
-const authRoutes = require('./routes/authRoutes');
 const scholarshipRoutes = require('./routes/scholarshipRoutes');
+const authRoutes = require('./routes/authRoutes'); 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
+
 app.use('/api/scholarships', scholarshipRoutes);
 
+
 const PORT = process.env.PORT || 5000;
-
-
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
